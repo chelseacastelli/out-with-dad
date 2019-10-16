505e65ce957765f5d7b2e1f687f0c378c8911c18
chelseacastelli <chelsea.castelli@students.makeschool.com>
2019-10-16 13:31:32 -0700

    Add MongoDB and Review model

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.Reviews
+reviews = db.reviews
 
 app = Flask(__name__)
 
 
-reviews = [
-    { 'restaurant': 'DeLuca\'s Italian Restaurant', 'address': '7324 Amboy Rd, Staten Island, NY 10307', 'review': 'The best meatballs I ever had. Drinks are also really good', 'rating': '5' },
-    { 'restaurant': 'Maizal Restaurant & Tequila Bar', 'address': '990 Bay St, Staten Island, NY 10305', 'review': 'One of my favorite places.. best tableside guac on SI, hands down!', 'rating': '5' }
-]
+# reviews = [
+#     { 'restaurant': 'DeLuca\'s Italian Restaurant', 'address': '7324 Amboy Rd, Staten Island, NY 10307', 'review': 'The best meatballs I ever had. Drinks are also really good', 'rating': '5' },
+#     { 'restaurant': 'Maizal Restaurant & Tequila Bar', 'address': '990 Bay St, Staten Island, NY 10305', 'review': 'One of my favorite places.. best tableside guac on SI, hands down!', 'rating': '5' }
+# ]
 
 
 @app.route('/')
 def index():
     """Return homepage."""
-    return render_template('index.html', reviews=reviews)
+    return render_template('index.html', reviews=reviews.find())
 
 
