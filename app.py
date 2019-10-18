a1e0da0067cd4ccdcd3f0dc4cacfe3ecb60a310a
chelseacastelli <chelsea.castelli@students.makeschool.com>
2019-10-17 21:58:57 -0700

    Add forms and more data

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, request, session, redirect
 from pymongo import MongoClient
-# from flask_bcrypt import Bycrypt
+from bson.objectid import ObjectId
 import os
 
 
@@ -18,13 +18,15 @@ reviews.drop()
 db.reviews.insert_many([
     { 'restaurant': 'Maizal Restaurant & Tequila Bar', 'address': '990 Bay St, Staten Island, NY 10305', 'review': 'The best tableside guac, hands down', 'rating': '5', 'image': './static/maizal.png' },
     { 'restaurant': 'Jose Tejas', 'address': '700 US-1, Iselin, NJ 08830', 'review': 'Cheap, delicious, and fun!', 'rating': '5', 'image': './static/jose.png' },
-    { 'restaurant': 'Pizzeria Giove', 'address': '278 New Dorp Lane, Staten Island, NY 10306', 'review': 'If you\'re looking for a delicious specialty pie, this is the place', 'rating': '5', 'image': './static/pg.png' },
     { 'restaurant': 'DeLuca\'s Italian Restaurant', 'address': '7324 Amboy Rd, Staten Island, NY 10307', 'review': 'Come for the drinks, stay for the meatballs', 'rating': '5', 'image': './static/delucas.png' },
-    { 'restaurant': 'Richmond Republic', 'address': '4459 Amboy Rd, Staten Island, NY 10312', 'review': 'Brunch = A Bloody Mary bar', 'rating': '3', 'image': './static/rr.png' },
     { 'restaurant': 'Patrizia\'s of Staten Island', 'address': '4255 Amboy Rd, Staten Island, NY 10308', 'review': 'One of my favorite Italian restaraunts. The burrata!', 'rating': '5', 'image': './static/pats.png' },
+    { 'restaurant': 'Pizzeria Giove', 'address': '278 New Dorp Lane, Staten Island, NY 10306', 'review': 'If you\'re looking for a delicious specialty pie, this is the place', 'rating': '5', 'image': './static/pg.png' },
     { 'restaurant': 'Ocean Sushi', 'address': '20 Jefferson Blvd, Staten Island, NY 10312', 'review': 'Fresh sushi & great service', 'rating': '4', 'image': './static/ocean.png' },
     { 'restaurant': 'Tommy\'s Tavern + Tap', 'address': '2655 Richmond Ave, Staten Island, NY 10314', 'review': 'Their wings are the BOMB.', 'rating': '4', 'image': './static/tommys.png' },
-    { 'restaurant': 'Cucina Fresca', 'address': '2110 Richmond Rd, Staten Island, NY 10306', 'review': '4', 'rating': 'The best service I\'ve ever gotten at a restaurant', 'image': './static/cucina.png' }
+    { 'restaurant': 'Cucina Fresca', 'address': '2110 Richmond Rd, Staten Island, NY 10306', 'review': 'The best service I\'ve ever gotten at a restaurant', 'rating': '4', 'image': './static/cucina.png' },
+    { 'restaurant': 'Richmond Republic', 'address': '4459 Amboy Rd, Staten Island, NY 10312', 'review': 'Brunch = A Bloody Mary bar', 'rating': '3', 'image': './static/rr.png' },
+    { 'restaurant': 'Mizu Japanese Restaurant', 'address': '240 Page Ave, Staten Island, NY 10307', 'review': 'The hibachi entrees are delicious', 'rating': '4', 'image': './static/mizu.png'},
+    { 'restaurant': 'Violette\'s Cellar', 'address': '2271 Hylan Blvd, Staten Island, NY 10306', 'review': 'The decor of this place is one of my favorites. Food is tapas style and pretty good.', 'rating': '3.5', 'image': './static/vc.png'}
 ])
 
 @app.route('/')
@@ -36,13 +38,34 @@ def index():
 def about():
     return render_template('about.html')
 
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
+@app.route('/submit')
+def restaurant_new():
+    """User adds new restaurant."""
+    return render_template('partials/submit.html', review={})
+
+@app.route('/restaurants', methods=['POST'])
+def restaurants_submit():
+    """Submit new restaurant."""
+    review = {
+        'restaurant': request.form.get('restaurant'),
+        'address': request.form.get('address'),
+        'rating': request.form.get('rating'),
+        'review': request.form.get('review'),
+        'image': request.form.get('image')
+
+    }
+    review_id = reviews.insert_one(review).inserted_id
+    return redirect(url_for('index', review_id=review_id))
+
+@app.route('/restaurants/<review_id>')
+def restaurants_show(review_id):
+    review = reviews.find_one({'_id': ObjectId(review_id)})
+    return render_template('show.html', review=review)
+
 
 @app.route('/account')
 def account():
-    pass
+    return render_template('partials/reg_log_form.html')
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
